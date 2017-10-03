e32b2dac8923683d66cf43a569959ad48db2f6d8
WeiChien Hsu <weichien711@gmail.com>
2017-10-02 23:32:25 -0500

    add styling

--- ToDo List Project/assets/js/todos.js
+++ ToDo List Project/assets/js/todos.js
@@ -1,4 +1,19 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(){
+	$(this).parent().fadeOut("500",function(){
+		$(this).remove();
+	})
+});
+
+$('input[type="text"]').keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append('<li><span><i class="fa fa-trash" aria-hidden="true"></i> </span>'+todoText+'</li>')
+
+	}
 });

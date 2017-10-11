24154b98e3fe0e18e49cbe7f71186ae6b361e54b
WeiChien Hsu <weichien711@gmail.com>
2017-10-10 19:40:54 -0500

    finish

--- Tic_Tac_Toe/js/tic.js
+++ Tic_Tac_Toe/js/tic.js
@@ -5,11 +5,15 @@ var choseId;
 // var turn = player2;
 var aiRecord;
 var turn;
+var playerChose=[];
+
+
 
 chooseOX();
 
-function checkPlayer(){
-if(record.filter(function(x){return x=="O"}).length > record.filter(function(x){return x=="X"}).length ){
+
+function checkPlayer(turn){
+if(turn == player1 ){
 		return player2;
 	}else{
 		return player1;
@@ -17,14 +21,18 @@ if(record.filter(function(x){return x=="O"}).length > record.filter(function(x){
 }
 
 function chooseOX(){
-	$("table,#startGame,#replay").hide();
+	$("table,#startGame,#replay,#win,#lose,#tie").hide();
 	//choose O
 	$("#O").on("click",function(){
-		
+		playerChose.push("O"); 
 		$("#startGame").show();
 		return turn = player1;
-
-	})
+	});
+	$("#X").on("click",function(){
+		playerChose.push("X"); 
+		$("#startGame").show();
+		return turn = player2;
+	});
 
 
 	//choose X
@@ -32,6 +40,7 @@ function chooseOX(){
 }
 
 $(".cell").on("click",function(){
+	whoWin();
 	// var turn = player1; //choose O or X to start;
 	choseId = $(this).attr('id');
 	$(this).text(turn);
@@ -39,12 +48,19 @@ $(".cell").on("click",function(){
 	// player play
 	aiRecord = record.filter(function(x){return x!=="O" && x!=="X"});
 	// give a new array for ai chooseing
-	turn = checkPlayer();
-	var random = aiRecord[Math.floor(Math.random()*(aiRecord.length))];
+	turn = checkPlayer(turn);
+	// tie situation 
+	if (aiRecord.length == 0){
+		tie();
+	} else{
+		var random = aiRecord[Math.floor(Math.random()*(aiRecord.length))];
+		record[random] = turn;
+		$(".cell").eq(random).text(turn);
+	}
+	turn = checkPlayer(turn);
+	// check if someone won!!
+	whoWin(); // check if anyone win
 	
-	record[random] = turn;
-	$(".cell").eq(random).text(turn);
-	turn = checkPlayer();
 });
 
 
@@ -58,6 +74,7 @@ $("#startGame").on("click",function(){
 	// need to reset the game
 	$("#startGame").hide();
 	$("#replay").show();
+	$(".cell").css("background-color","#4CAF50")
 })
 
 $("#replay").on("click",function(){
@@ -67,9 +84,57 @@ $("#replay").on("click",function(){
 });
 
 
-// function whoWin(){
-// 	if()
-// }
+function whoWin(){
 
+	if(record[0] == record[1] && record[1] == record[2]){
+		$("#0,#1,#2").css("background-color","red");
+		win(record[0]);
+	} else if(record[3] == record[4] && record[4] == record[5]){
+		$("#3,#4,#5").css("background-color","red");
+		win(record[3]);
+	} else if(record[6] == record[7] && record[7] == record[8]){
+		$("#6,#7,#8").css("background-color","red");
+		win(record[6]);
+	} else if(record[0] == record[3] && record[3] == record[6]){
+		$("#0,#3,#6").css("background-color","red");
+		win(record[0]);
+	} else if(record[1] == record[4] && record[4] == record[7]){
+		$("#1,#4,#7").css("background-color","red");
+		win(record[1]);
+	} else if(record[2] == record[5] && record[5] == record[8]){
+		$("#2,#5,#8").css("background-color","red");
+		win(record[2]);
+	} else if(record[0] == record[4] && record[4] == record[8]){
+		$("#0,#4,#8").css("background-color","red");
+		win(record[0]);
+	} else if(record[2] == record[4] && record[4] == record[6]){
+		$("#2,#4,#6").css("background-color","red");
+		win(record[2]);
+	}
+	
+} // whoWin
+
+function win(opt){
+	console.log(playerChose);
+	if(playerChose[0] == opt){
+		$("#win").show("slow");
+	}else{
+		$("#lose").show("slow");
+	}
+}
+
+function tie(){
+	$("#tie").show("slow");
+}
+// WinCondition = [
+// 	["O","O","O",3,4,5,6,7,8],
+// 	[0,1,2,"O","O","O",6,7,8],
+// 	[0,1,2,3,4,5,"O","O","O"],
+// 	["O",1,2,"O",4,5,"O",7,8],
+// 	[0,"O",2,3,"O",5,6,"O",8],
+// 	[0,1,"O",3,4,"O",6,7,"O"],
+// 	["O",1,2,3,"O",5,6,7,"O"],
+// 	[0,1,"O",3,"O",5,"O",7,8],
+// ]
 
 
